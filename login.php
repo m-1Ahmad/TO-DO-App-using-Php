79354e054c9052a906e8db040085ca53867d9d26
m-1Ahmad <m.ahmad.ahmad@outlook.com>
2024-09-10 16:25:46 +0500

    Add login.php

---
+++ login.php
@@ -0,0 +1,58 @@
+<?php
+session_start();
+require_once 'user.php';
+if(isset($_SESSION['loggedin']) && $_SESSION['loggedin']===true){
+    header('location: homepage.php');
+    exit;
+}
+$user = new User();
+$error = [];
+if($_SERVER['REQUEST_METHOD']=='POST'){
+    $email = htmlspecialchars(trim($_POST['Email']));
+    $pass = $_POST['Password'];
+    $error = $user->loggedin($email,$pass);
+    if(empty($error)){
+        $_SESSION['loggedin'] = true;
+        $_SESSION['email'] = $email;
+        $_SESSION['id'] = $user->userid($email);
+        $_SESSION['name'] = $user->username($email);
+        header('Location: homepage.php');
+        exit();
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="login-container">
+        <h2>Login</h2>
+        <form method = "post">
+            <input type="email" name = "Email" placeholder="Email"
+            value=<?php echo !empty($_POST['Email']) ? htmlspecialchars($email) : '';?>>
+            <?php
+                if(!empty($error['emailError'])){
+                    echo '<span class= "error-message">'. $error['emailError'] . '</span>'; 
+                }
+            ?>
+            <input type="password" name = "Password" placeholder="Password">
+            <?php
+                if(!empty($error['passwordError'])){
+                    echo '<span class= "error-message">'. $error['passwordError'] . '</span>'; 
+                }
+            ?>
+            <input type="submit" value="Login">
+        </form>
+        <div class="signup-link">
+          <p>Don't have an account? <a href="signup.php">Sign up here</a></p>
+      </div>
+    </div>
+    
+</body>
+</html>
